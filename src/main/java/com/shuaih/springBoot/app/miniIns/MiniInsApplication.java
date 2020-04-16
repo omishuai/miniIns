f9ec938602052e174736d3e9a0a5a84b6b885ccf
Shuai Huang <shhuang9532@gmail.com>
2020-04-15 22:57:39 -0700

    init

---
+++ src/main/java/com/shuaih/springBoot/app/miniIns/MiniInsApplication.java
@@ -0,0 +1,13 @@
+package com.shuaih.springBoot.app.miniIns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniInsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniInsApplication.class, args);
+	}
+
+}
