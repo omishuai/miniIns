4f403d374e60cd452ed1596426cf2d386d007240
Shuai Huang <shhuang9532@gmail.com>
2020-04-17 16:22:05 -0700

    move logic from controller  to userService

--- src/main/java/com/app/miniIns/controllers/MyController.java
+++ src/main/java/com/app/miniIns/controllers/MyController.java
@@ -5,15 +5,13 @@ import com.app.miniIns.daos.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 
-@Controller
+@RestController
 public class MyController {
 
     public UserService getUserService() {
@@ -50,23 +48,8 @@ public class MyController {
     }
 
     @PostMapping(path = "/register")
-    public ModelAndView register(ModelAndView modelAndView, @Valid User user, BindingResult bindingResult) {
-        User existingUser = userService.findByEmail(user.getEmail());
-
-        if (existingUser != null) {
-            System.out.println(existingUser + " EXISTS! ");
-            modelAndView.addObject("alreadyRegisteredMessage", "Oops!  There is already a user registered with the email provided.");
-            modelAndView.setViewName("register");
-            bindingResult.reject("err", "Oops!  There is already a user registered with the email provided.");
-        }
-
-        if (bindingResult.hasErrors()) {
-            modelAndView.setViewName("register");
-        } else {
-            userService.addUser(user);
-            return new ModelAndView("redirect:/" + user.getUsername());
-        }
-        return modelAndView;
+    public User register(@Valid User user) throws Exception {
+        return userService.addUser(user);
     }
 
 
--- src/main/java/com/app/miniIns/daos/UserService.java
+++ src/main/java/com/app/miniIns/daos/UserService.java
@@ -19,29 +19,27 @@ public class UserService {
     private UserRepository userRepo;
 
 
-    public User addUser(String username, String password, String email, int age, String gender) {
-        try {
-            User n = new User(username, password, email, age, gender);
-            userRepo.save(n);
-            return n;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+//    public User addUser(String username, String password, String email, int age, String gender) {
+//        try {
+//            User n = new User(username, password, email, age, gender);
+//            userRepo.save(n);
+//            return n;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//    }
 
     public User findByEmail(String email) {
         return userRepo.findByEmail(email);
     }
 
-    public User addUser(User user) {
-        try {
-            userRepo.save(user);
-            return user;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+    public User addUser(User user) throws Exception {
+        //validate the user:
+        if (findByEmail(user.getEmail()) != null) throw new Exception("Existing User");
+        if (user.getPassword().length() < 8) throw new Exception("Password Is Less Than 8 Characters");
+        if (user.getAge() < 18) throw new Exception("Under Age 18");
+        return userRepo.save(user);
     }
 
 }
