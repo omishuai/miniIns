82e32de36cf43b9f53c80f6034d0aecc02f2662b
Shuai Huang <shhuang9532@gmail.com>
2020-04-30 13:40:07 -0700

    add test profile and modified method signature

--- src/test/java/com/app/miniIns/cucumber/bdd/InMemoryS3Service.java
+++ src/test/java/com/app/miniIns/cucumber/bdd/InMemoryS3Service.java
@@ -1,6 +1,7 @@
 package com.app.miniIns.cucumber.bdd;
 
-import com.app.miniIns.services.AbstractS3;
+import com.app.miniIns.services.FileStorageService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,16 +10,16 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 @Service
-public class InMemoryS3Service implements AbstractS3 {
+@Profile("test")
+public class InMemoryS3Service implements FileStorageService {
 
-
-    @Override
-    public URL upload(String bucketName, String s3Key, MultipartFile file) throws IOException {
+    public URL upload(String s3Key, MultipartFile file) throws IOException {
         return new URL("http://www.google.com/"+s3Key);
     }
 
-    @Override
-    public URL getUrl(String bucket, String s3Key) throws MalformedURLException {
+    public URL getUrl(String s3Key) throws MalformedURLException {
         return new URL("http://www.google.com/"+s3Key);
     }
+
+
 }
